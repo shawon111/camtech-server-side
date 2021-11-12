9b5a5ea1b3f13da5cf20f470f38085525f69d19f
shawon ahmmed <shawonahmmed08509@gmail.com>
2021-11-13 03:02:17 +0600

    port updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require("mongodb");
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send("camtech server")
