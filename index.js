c0fff59322123bd3bf944c4234f19db4423408a3
shawon ahmmed <shawonahmmed08509@gmail.com>
2021-11-13 19:25:57 +0600

    post order api created

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("camtech server")
+  res.send("camtech server")
 })
 
 // connect with mongodb
@@ -16,32 +17,48 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("camtech_digital");
-      const cameraCollection = database.collection("cameras");
-      const reviewsCollection = database.collection("reviews");
-      
-      //get api for showing products
-      app.get('/products', async (req, res)=> {
-          const cursor = cameraCollection.find({});
-          const result = await cursor.toArray();
-          res.json(result);
-      })
+  try {
+    await client.connect();
+    const database = client.db("camtech_digital");
+    const cameraCollection = database.collection("cameras");
+    const reviewsCollection = database.collection("reviews");
+    const orderCollection = database.collection("orders");
 
-      //get api for showing reviews
-      app.get('/reviews', async (req, res)=> {
-        const cursor = reviewsCollection.find({});
-        const result = await cursor.toArray();
-        res.json(result);
-      })
+    //get api for showing products
+    app.get('/products', async (req, res) => {
+      const cursor = cameraCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    })
 
-    } finally {
+    //get api for showing reviews
+    app.get('/reviews', async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    })
+
+    //get api for finding a product
+    app.get('/products/:id', async (req, res) => {
+      const Id = req.params.id;
+      const query = { _id: ObjectId(Id) };
+      const result = await cameraCollection.findOne(query);
+      res.json(result)
+    })
+
+    //post api for creating new order
+    app.post('/orders/neworder', async (req, res)=>{
+      const doc = req.body;
+      const result = await orderCollection.insertOne(doc);
+      res.json(result);
+    })
+
+  } finally {
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log("listening to the port:", port)
+  console.log("listening to the port:", port)
 })
